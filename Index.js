2f1afd2791af53959e61bb79b0ef8f4686ff77df
Sonu Santhosh <sonusanthosh371@gmail.com>
2025-03-15 14:20:02 +0530

    Welcome

---
+++ Index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import ConnectDB from './DataBase.js';
+
+dotenv.config({
+    path: './.env'
+})
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+ConnectDB()
+.then(() => {
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}
+)
+.catch((error) => {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+}
+)
+
+
